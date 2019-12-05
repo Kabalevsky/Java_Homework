a58ca87fe0f90fd2353460a187be6008d76bae9a
Kabalevsky <v.kabalevsky@yandex.ru>
2019-12-05 15:27:49 +0300

    Исправлена задача 8

--- Homework 1/src/Task7.java
+++ Homework 1/src/Task7.java
@@ -1,6 +1,6 @@
 public class Task7 { //  Задание №7
     public static void main(String[] args) {
-        System.out.println (name("Аня"));
+        System.out.println(name("Аня"));
     }
 
     public static String name(String a) {
--- Homework 1/src/Task8.java
+++ Homework 1/src/Task8.java
@@ -1,17 +1,13 @@
 public class Task8 { //  Задание №8
     public static void main(String[] args) {
-        System.out.println(year(1984));
+        System.out.println(year(800));
     }
 
     public static String year(int a) {
-        if (a % 400 == 0) {
-            return ("Указан год " + a + ", этот год високосный.");
-        } else if (a % 100 == 0) {
-            return ("Указан год " + a + ", этот год не високосный.");
-        } else if (a % 4 == 0) {
-            return ("Указан год " + a + ", этот год високосный.");
+        if (a % 400 == 0 && a % 4 == 0) {
+            return "Указан год " + a + ", этот год високосный.";
         } else {
-            return ("Указан год " + a + ", этот год не високосный.");
+            return "Указан год " + a + ", этот год не високосный.";
         }
     }
 }
