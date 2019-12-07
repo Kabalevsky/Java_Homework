5f326ef5d2e09815deb316a58aa44f7ac50a894b
Kabalevsky <v.kabalevsky@yandex.ru>
2019-12-07 14:07:19 +0300

    Тестовая ветка

--- Homework2/src/Task4variant2.java
+++ Homework2/src/Task4variant2.java
@@ -1,6 +1,6 @@
 public class Task4variant2 { // Задание №4, вариант 2
     public static void main(String args[]) {
-        int[][] array = new int[5][5];
+        int[][] array = new int[6][6];
         for (int i = 0; i < array.length; i++) {
             for (int j = 0, k = array[i].length; j < array[i].length; j++, k--) {
                 if (i == j || i == (k + j)) array[i][j] = 1;
